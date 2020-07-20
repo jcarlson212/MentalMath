85f7a09ef7720d7a5f9225e7e9106a390738effb
Jason Carlson <jtcarlso@asu.edu>
2020-07-20 02:23:42 -0700

    Added alert for users not logged in and user points to model

--- MentalMathWebsite/models.py
+++ MentalMathWebsite/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
     username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=30)
     email = models.EmailField(max_length=30, unique=True)
-
+    points = models.IntegerField(default=0)
 
 
 class ThreadManager(models.Manager):
