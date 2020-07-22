0cd57f3f58af63989826e9227e8310e883719fef
Jason Carlson <jtcarlso@asu.edu>
2020-07-22 01:07:11 -0700

    Added environment variables

--- MentalMath/settings.py
+++ MentalMath/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rw9si7+rx$lvp@o-v%td=6=_g-9+5r+hn3-$lvowi8jof7)ny$'
+#SECRET_KEY = 'rw9si7+rx$lvp@o-v%td=6=_g-9+5r+hn3-$lvowi8jof7)ny$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mentalmathwebsite.herokuapp.com"]
 
 AUTH_USER_MODEL = "MentalMathWebsite.User" 
 
